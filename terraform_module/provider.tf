a4f1e0edfd3ff953dfc61db18bf8c904adb74d1a
Rishusharma1 <134528936+Rishusharma1@users.noreply.github.com>
2023-07-05 23:16:40 +0530

    Update provider.tf

--- terraform_module/provider.tf
+++ terraform_module/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "mubucketstate"
+    dynamodb_table = "dynamo"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 
