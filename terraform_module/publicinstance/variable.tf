f7cfb238738bdabedd3a164785d0ed4d3a02bf8d
Rishusharma1 <134528936+Rishusharma1@users.noreply.github.com>
2023-07-07 11:54:39 +0530

    Update variable.tf

--- terraform_module/publicinstance/variable.tf
+++ terraform_module/publicinstance/variable.tf
@@ -1,56 +1,7 @@
 
-# variable "sub_id" {
-#   type = string
-#   default = ""
-# }
-
-# variable "security_id" {
-#   type = string
-#   default = ""
-# }
-# # variable "privatesub_id"{
-# #   type = string
-# #   default = ""
-# # }
-
-
-# #-----------------------
-# # variable "inst_name" {
-# #   type    = list(string)
-# #   default = ["Instance_pub", "Instance_pvt"]
-# # }
-
-# # variable "static" {
-# #   type = map(any)
-# #   default = {
-# #     ami      = "ami-053b0d53c279acc90"
-# #     publicip = true
-# #     keyname  = "ubuntukey"
-# #     itype    = "t2.large"
-# #   }
-# # }
-
-
-
-
-# variable "sub_id" {
-#   type = string
-#   default = ""
-# }
-
-# variable "security_id" {
-#   type = string
-#   default = ""
-# }
-
-
-
-
-
-
 variable "public_name" {
   type    = string
-  default = "elasticsearch"
+  default = "elasticsearchpub"
 }
 # variable "p_inst_name" {
 #   type    = list(string)
@@ -65,10 +16,7 @@ variable "static" {
     itype    = "t2.large"
   }
 }
-# variable "pvt_subnet_id" {
-#   type = list(string)
-  
-# }
+
 
 variable "sub_id" {
   type = string
